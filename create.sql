1619a87b116afc1a8070620c8b2a50f3ddb20249
Robert <rdtheriault@yahoo.com>
2015-04-08 07:53:13 -0700

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,4 +1,4 @@
 BEGIN;
-CREATE TABLE filed (id Integer Primary Key, file Text, name Text, uploaded Date, types Text); 
+CREATE TABLE files (id Integer Primary Key, file Text, name Text, uploaded Date, types Text); 
 COMMIT; 
 .exit
