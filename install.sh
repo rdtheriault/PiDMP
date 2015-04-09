1bb066a60979c513e68c5979554036583876c261
Robert <rdtheriault@yahoo.com>
2015-04-09 07:49:32 -0700

    Update install.sh
    Added information text for user and quieted updates and installs

--- install.sh
+++ install.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-sudo apt-get upgrade -y
-sudo apt-get update -y
-sudo apt-get install sqlite3 -y
-sudo apt-get install apache2 -y
-sudo apt-get install php5 php5-sqlite -y 
-sudo apt-get install xRDP -y 
-sudo apt-get install x11vnc -y 
-sudo apt-get install tightvncserver -y 
-sudo apt-get install libreoffice -y
-sudo apt-get install xscreensaver -y
+echo "***Starting update***"
+sudo apt-get update -qq
+sudo apt-get dist-upgrade -qq
+sudo apt-get upgrade -qq
+sudo apt-get update -qq
+echo "***Completed Update***"
 
+echo "***Starting Installs***"
+echo "sqlite3"
+sudo apt-get install sqlite3 -qq
+echo "apache2"
+sudo apt-get install apache2 -qq
+echo "php5"
+sudo apt-get install php5 php5-sqlite -qq
+echo "VNC programs"
+sudo apt-get install xRDP -qq 
+sudo apt-get install x11vnc -qq
+sudo apt-get install tightvncserver -qq
+echo "libreoffice"
+sudo apt-get install libreoffice -qq
+echo "Screensaver"
+sudo apt-get install xscreensaver -qq
+echo "***Completed installing programs***"
+
+echo "->Starting Impress to create config file, close program and PiDMP install will continue"
+sleep 5
 libreoffice --impress
 
+echo "***Get Templates***"
 sudo wget http://sagefirellc.com/PiDMP/template.odp
 sudo wget http://sagefirellc.com/PiDMP/template.pptx
+echo "***Templated retreived***"
 
+echo "***Create directories and move files***"
 mkdir /home/pi/temp
 mkdir /home/pi/temp/pres
 sudo mkdir /var/www/templates
@@ -34,10 +50,15 @@ sudo mv -f .xscreensaver /home/pi
 sudo mv template.opd /var/www/templates
 sudo mv template.pptx /var/www/templates 
 sudo mv info.txt /var/www/templates/info.txt
+echo "***Completed creates and moves***"
 
+echo "->Creating database, please enter '.read /home/pi/create.sql'"
+echo "then use '.exit' to continue installation"
 cd /home/pi/temp
 sqlite3 main.db
 
+echo "***Changing Permissions***"
 sudo chown -R www-data /home/pi/temp
 sudo chmod 777 /home/pi/temp/main.db
 sudo chmod +x /home/pi/temp/OpenShow.sh
+echo "Change Complete"
