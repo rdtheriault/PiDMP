3e7a24eb8730aaf0c4d110e989a7c0901a07c579
Robert <rdtheriault@yahoo.com>
2015-04-10 12:36:20 -0700

    Create startvideo.sh
    Starts loop of video, shuts off only when libreoffice is started.

---
+++ startvideo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pkill soffice.bin
+
+
+while true; do
+if pgrep omxplayer > /dev/null
+then
+	echo 'running'
+else
+	omxplayer $1 -display :0.0
+fi
+if ps ax | grep -v grep | grep libreoffice > /dev/null then
+    break
+fi
+done
