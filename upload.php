bf44eeb57fbb4fd9dd9db8781eee701fb1b89152
Robert <rdtheriault@yahoo.com>
2015-04-11 20:40:08 -0700

    Update upload.php
    Added logic to check file type and add type to database input.

--- upload.php
+++ upload.php
@@ -14,32 +14,65 @@
         return $text;
     }
 
+  function seperate($text, $type){
+    $array = explode('.', $text);
+    if ($type == "file"){return $array[0];}
+    if ($type == "type"){return $array[1];}
+  }
+
+
+    function checkForFile($path, $text){
+        $exists = file_exists($path);
+    $typ = seperate($text, "type");
+    $name = seperate($text, "file");
+    $targ = seperate($path, "file");
+    if (!$exists){return $text;}
+    else {
+        while($exists) {
+            $name = $name."A";//update name
+            $targ = $targ."A";//update target
+            $final = $name.".".$typ;
+            $test = $targ.".".$typ;
+            $exists = file_exists($test);
+            }
+        return $final;
+    }
+    }
+
+  function checkType($text){
+    $type = seperate($text, "type");
+    if ($type == "odp" OR $type == "pps"){return "pres";}
+    elseif ($type == "mp4" OR $type == "mkv"){return "vid";}
+    else {return "none";}
+  }
+
     //echo "test";
     $title = $_POST['title'];
-    //$var = $_POST['file'];
-    //$var = $var + '.odp';
+
     if ($_POST['title'] != "")
     {
         if ($_POST['MAX_FILE_SIZE'] == "10000000")
         {
             // Where the file is going to be placed
             $target_path = "/home/pi/temp/pres/";
-            //$target_path = "files/";
             $dat = date('Y-m-d');
             //echo $dat;
 
             $fileName = $_FILES['uploadedfile']['tmp_name'];
-            $newFileName = replaceAll(basename( $_FILES['uploadedfile']['name']));
+            $newFileName = replaceAll(basename( $_FILES['uploadedfile']['name']));  //gets rid of spaces
+        $type = checkType($newFileName);  //get the file type (returning none if not accepted)
+        
 
             //echo $fileName."<br>";
-            /* Add the original filename to our target path.
-            Result is "uploads/filename.extension" */
-            $target_path = $target_path . $newFileName;
-            //echo $target_path."<br>";
+            $target_test = $target_path . $newFileName;//get path to use to check for file
+        $newFileName = checkForFile($target_test, $newFileName);  //check if files names exist and adds A to end until it does not
+        $target_path = $target_path . $newFileName;//put together final name
 
+
+            //echo $target_path."<br>";
             //echo var_dump($_FILES);
 
-            if(move_uploaded_file($fileName, $target_path))
+            if(move_uploaded_file($fileName, $target_path) AND $type != "none")
             {
                 echo "The file ".$newFileName." has been uploaded <br>";
                 try{
@@ -49,10 +82,14 @@
                     echo $e->getMessage();
                 }
 
-                $sql = "INSERT INTO files (file, name, uploaded, types) VALUES ('".$target_path."','".$title."','".$dat."','type')";
+                $sql = "INSERT INTO files (file, name, uploaded, types) VALUES ('".$target_path."','".$title."','".$dat."','".$type."')";
                 //echo $sql;
                 $err = $dbc->query($sql);
             } 
+        elseif ($type == "none")
+        {
+        echo "You did not use an allowed file type, please use .odp or .pps for presentations or .mp4 or .mkv for videos";
+        }
             else
             {
                 echo "There was an error uploading the file, please try again!<br>";
