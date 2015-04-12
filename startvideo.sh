da7632f2fcf6d129caf5b23f4cf0d4119547a103
Robert <rdtheriault@yahoo.com>
2015-04-11 20:33:10 -0700

    Update startvideo.sh

--- startvideo.sh
+++ startvideo.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+
+libreoffice
+pkill omxplayer
+sleep 5
 pkill soffice.bin
 
 
 while true; do
 if pgrep omxplayer > /dev/null
 then
-	echo 'running'
+    echo 'running'
 else
-	omxplayer $1 -display :0.0
+    omxplayer $1 -display :0.0
 fi
-if ps ax | grep -v grep | grep libreoffice > /dev/null then
+if ps ax | grep -v grep | grep libreoffice > /dev/null
+then
     break
 fi
 done
