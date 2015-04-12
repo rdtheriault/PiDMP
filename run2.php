555094b6d3ac1b3604a14bae51cad813e2cd8dc4
Robert <rdtheriault@yahoo.com>
2015-04-11 20:36:17 -0700

    Update run2.php
    Added logic for selecting different types of input.

--- run2.php
+++ run2.php
@@ -2,24 +2,34 @@
 <body>
 
 <?php 
-	//echo "test";
-	//$output = shell_exec('/home/pi/Desktop/OpenShow.sh');
-	//$output = shell_exec('whoami');
-	//$command = escapeshellcmd('/home/pi/Desktop/OpenShow.py');
-	//$output = shell_exec($command);
-	//$output = exec('sudo python /home/pi/Desktop/OpenShow.py');
+    //echo "test";
+    //$output = shell_exec('/home/pi/Desktop/OpenShow.sh');
+    //$output = shell_exec('whoami');
+    //$command = escapeshellcmd('/home/pi/Desktop/OpenShow.py');
+    //$output = shell_exec($command);
+    //$output = exec('sudo python /home/pi/Desktop/OpenShow.py');
 
-	$var = $_POST["file"];
+    $var = $_POST["file"];
+    $var2 = $_POST["type"];
+    $var3 = $_POST["stream"];
 
-	$output = exec('sudo -u pi /home/pi/temp/OpenShow.sh '.$var);
-	//$output = exec('/home/pi/OpenShow.sh '.$var);
-	echo $output;
-	//$user = get_current_user();
-	//echo $user;
-	echo "<br>";
-	echo $var;
+    if ($var2 == "pres"){
+        $output = exec('sudo -u pi /home/pi/temp/OpenShow.sh '.$var);
+    }
+    elseif ($var2 == "vid"){
+        $output = exec('sudo -u pi /home/pi/temp/startvideo.sh '.$var);
+    }
+    elseif ($var3 == "stream"){
+        echo "Yep this would be a stream";
+    }
 
-	
+    //$output = exec('/home/pi/OpenShow.sh '.$var);
+    echo $output;
+    //$user = get_current_user();
+    //echo $user;
+    echo $var;
+
+    
 ?>
 
 </body>
