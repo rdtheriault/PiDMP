6c8b2e880cd8879f317491cec9da2f57a7359aa8
Robert <rdtheriault@yahoo.com>
2015-04-23 07:30:26 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get install sudo
+
 echo "***Starting update (this might take a while)***"
 sudo apt-get update -qq
 sudo apt-get dist-upgrade -qq
@@ -34,6 +36,8 @@ sudo wget http://sagefirellc.com/PiDMP/template.pptx
 echo "***Templated retreived***"
 
 echo "***Create directories and move files***"
+mkdir /home
+mkdir /home/pi
 mkdir /home/pi/temp
 mkdir /home/pi/temp/pres
 sudo mkdir /var/www/templates
@@ -48,25 +52,13 @@ sudo mv OpenShow.sh /home/pi/temp
 sudo mv startvideo.sh /home/pi/temp
 sudo mv startvideos.sh /home/pi/temp
 sudo mv stream.sh /home/pi/temp
-sudo mv -f Module1.xba /home/pi/.config/libreoffice/3/user/basic/Standard
-sudo mv -f registrymodifications.xcu /home/pi/.config/libreoffice/3/user
-sudo mv -f .xscreensaver /home/pi
+sudo mv -f Module1.xba /root/.config/libreoffice/3/user/basic/Standard
+sudo mv -f registrymodifications.xcu /root/.config/libreoffice/3/user
+sudo mv -f .xscreensaver /root
 sudo mv -f php.ini /etc/php5/apache2
 sudo mv template.odp /var/www/templates
 sudo mv template.pptx /var/www/templates 
 sudo mv info.txt /var/www/templates/info.txt
-echo ""
-echo "Do you want to disable overscan (do you see a black border)?"
-echo "You can make future changes in /boot/config.txt"
-echo "Enter the number that matches your choice."
-select yn in "Yes" "No"
-do
-case $yn in
-    Yes ) sudo mv -f config.txt /boot; break;;
-    No ) break;;
-esac
-done
-
 echo "***Completed creates and moves***"
 
 echo "->Creating database, please enter '.read /home/pi/create.sql'"
